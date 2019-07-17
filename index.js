a52a8fc4f16f192a70f4ccc4908e2af3bef0f03f
energetic-port-0799 <m.tarrats@yahoo.com>
2019-07-17 22:38:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
-function (happyHolidays){
-  switch (happyHolidays) {
+function happyHolidays(){
+  switch ("happyHolidays") {
     case "Happy holidays!":
       return "Happy holidays!"
       break;

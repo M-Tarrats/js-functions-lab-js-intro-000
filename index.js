09cf2b305666ba75a2d04e968640ff14f4349d38
energetic-port-0799 <m.tarrats@yahoo.com>
2019-07-17 22:40:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 // write your code below!
 function happyHolidays(){
-  switch ("happyHolidays") {
-    case "Happy holidays!":
-      return "Happy holidays!"
-      break;
-    default:
-
-  }
+  
 }

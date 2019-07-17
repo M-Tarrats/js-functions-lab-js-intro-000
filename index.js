ff529ef1c2dadb094180d9aeef3d24e95c3db5bf
energetic-port-0799 <m.tarrats@yahoo.com>
2019-07-17 23:07:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyHolidaysTo(name) {
 }
 
 function happyHolidayTo(holiday, name) {
-  return "Happy Independence day, you!"
+  return "Happy Independence Day, you!"
 }
 
 function holidayCountdown(holiday, days) {
